460281c45b2d8bdca064df3d8b0477ca2737a0ce
naseokju <lukas95@naver.com>
2023-10-15 18:22:59 +0900

    react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import NavBar from './layout/NavBar'
+import Footer from './layout/Footer'
+import LandingPage from './pages/LandingPage'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+
+function Layout() {
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <h1 className='underline font-bold text-3xl'>
-      Hello World!
-    </h1>
+    <Routes>
+      <Route path='/' element={<Layout />} >
+        <Route index element={<LandingPage />} />
+
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        
+      </Route>
+    </Routes>
   )
 }
 
