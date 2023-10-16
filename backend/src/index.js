8c56509a870bf33f0bf91e73800cd95a55cb3096
naseokju <lukas95@naver.com>
2023-10-16 23:52:20 +0900

    MongoDB 사용

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 const port = 4000;
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('연결 완료');
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
 app.get('/', (req, res) => {
     res.send('안녕하세요!');
 })
 
-app.post('/', (req, res) => {
+app.post('/', (req, res) => { 
     console.log(req.body);
     res.json(req.body);
 })
