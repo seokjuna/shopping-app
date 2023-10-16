58cde1a08f78fe3f0ffea1189d6c5302c6023d47
naseokju <lukas95@naver.com>
2023-10-16 23:19:25 +0900

    express.json()

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,20 @@ const cors = require('cors');
 
 const app = express();
 const port = 4000;
-app.use(cors());
 
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('안녕하세요!');
 })
 
-app.use(express.static(path.join(__dirname, '../uploads')));
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+})
 
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.listen(4000, () => {
     console.log(`${port}번에서 실행이 되었습니다.`);
