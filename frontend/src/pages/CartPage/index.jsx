03a8fbd00b6163c8feb1cd086f8b85f10e75b987
naseokju <lukas95@naver.com>
2023-10-23 16:40:45 +0900

    CartPage 생성

--- frontend/src/pages/CartPage/index.jsx
+++ frontend/src/pages/CartPage/index.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCartItems } from '../../store/thunkFunctions';
 
 const CartPage = () => {
     const userData = useSelector(state => state.user?.userData);
+    const cartDetail = useSelector(state => state.user?.cartDetail);
     const dispatch = useDispatch();
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         let cartItemIds = [];
@@ -21,8 +23,37 @@ const CartPage = () => {
         } 
     }, [dispatch, userData]);
 
+    useEffect(() => {
+        calculateTotal(cartDetail);
+    }, [cartDetail]);
+
+    const calculateTotal = (cartItems) => {
+        let total = 0;
+        cartItems.map(item => total += item.price * item.quantity);
+        setTotal(total);
+    };
+
     return (
-        <div>CartPage</div>
+        <section>
+            <div className='text-center m-7'>
+                <h2 className='text-2xl'>나의 장바구니</h2>
+            </div>
+
+            {cartDetail?.length > 0 ?
+                <>
+                    <div className='mt-10'>
+                        <p><span className='font-bold'>합계:</span> {total}원</p>
+                        <button
+                            className='text-white bg-black px-4 py-2 mt-5 rounded-md hover:bg-gray-500'
+                        >
+                            결제하기
+                        </button>
+                    </div>
+                </>
+                :
+                <p>장바구니가 비었습니다.</p>
+            }
+        </section>
     )
 }
 
